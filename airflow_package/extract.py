97d8623ac7fd1c9cfdd465b83c729907d8b98789
rutvikshah <rutvikjshah@gmail.com>
2023-09-30 15:10:50 +0530

    project-uber-DA-001 : Pushed all codebase

---
+++ airflow_package/extract.py
@@ -0,0 +1,22 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+
+    url = 'https://storage.googleapis.com/uber-data-engineering-project-darshil/uber_data.csv'
+    response = requests.get(url)
+
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@test
+def test_output(output, *args) -> None:
+
+    assert output is not None, 'The output is undefined'
